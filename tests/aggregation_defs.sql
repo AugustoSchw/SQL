edb2b54fba8a6bf410364214e423b7d5723c0209
AugustoSchw <augustoschweitzer2@gmail.com>
2024-12-14 18:09:10 -0300

    aggregation functions

---
+++ tests/aggregation_defs.sql
@@ -0,0 +1,13 @@
+-- AVG() -> returns average value in floating point value (2.342423)
+--		ROUND(input_to_round, number_of_precision) -> specify precision after the decimal
+
+-- COUNT() 
+-- MAX()
+-- MIN()
+-- SUM -> returns the sum of all values
+
+-- Aggregate functions ONLY work in SELECT or HAVING clause
+
+--SELECT ROUND(AVG(amount), 2) FROM payment; -- round up to 2 decimals places
+
+--SELECT MIN(replacement_cost) FROM film;
